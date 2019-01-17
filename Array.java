cde07ca5c9c346b395a65f49134db98375616068
emrekn <46498401+emrekn@users.noreply.github.com>
2019-01-17 13:07:03 +0300

    Update Array.java

--- Array.java
+++ Array.java
@@ -5,7 +5,15 @@ package javaogrenme2;
 
 public class Main {
 	public static void main(String args[]) {
+  //Diziler iki şekilde de tanımlanabilir
+  int a [] = new int [5]; // a dizisine 5 tane değişken alabileceğini söyledik.
   int[] dizi = {10, 20, 30, 40};
+  
+  a[0] = 1;
+  a[1] = 2;
+  a[2] = 3;
+  a[3] = 4;
+  a[4] = 5;
   System.out.println(dizi[0]);
 	}
 }
