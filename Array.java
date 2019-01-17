ce2a31e691f111aa4fa0437334684593091070df
emrekn <46498401+emrekn@users.noreply.github.com>
2019-01-17 13:00:33 +0300

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,11 @@
+//Create: Emre EMANET
+//Date: 1/17/2019
+//Name: Array
+package javaogrenme2;
+
+public class Main {
+	public static void main(String args[]) {
+  int[] dizi = {10, 20, 30, 40};
+  System.out.println(dizi[0]);
+	}
+}
