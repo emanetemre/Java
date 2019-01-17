c630a7358cfdda2e05a0cbb1b7f077d20c87d354
emrekn <46498401+emrekn@users.noreply.github.com>
2019-01-17 13:10:56 +0300

    Update Array.java

--- Array.java
+++ Array.java
@@ -14,6 +14,8 @@ public class Main {
   a[2] = 3;
   a[3] = 4;
   a[4] = 5;
-  System.out.println(dizi[0]);
+		
+  System.out.println(""+a[0]) //a dizisinin ilk değişkenini yaz dedik.
+  System.out.println(dizi[0]+" "+dizi[1]+" "+dizi[2]+" "+dizi[3]); //Dizinin tüm değişkenlerini yazdırdık.
 	}
 }
