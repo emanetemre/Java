c7df5f011e1bb65b76427ac2e30eb2b91f155486
emrekn <46498401+emrekn@users.noreply.github.com>
2019-01-18 14:38:13 +0300

    Create Set.java

---
+++ Set.java
@@ -0,0 +1,21 @@
+//Create: Emre EMANET
+//Date: 1/17/2019
+//Name: Array
+package javaogrenme1;
+
+import java.util.HashSet;
+
+public class Main {
+	public static void main(String args[]) {
+		//Set
+		
+		HashSet<String> mySet = new HashSet<String>();
+		
+		mySet.add("Ali");
+		mySet.add("Ali");
+		
+		System.out.println(mySet.size());
+		
+
+	}
+}
