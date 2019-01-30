d64e036c5144caa1a180d7c39b31c65ce64573f0
emrekn <46498401+emrekn@users.noreply.github.com>
2019-01-30 15:36:25 +0300

    Add files via upload

---
+++ 030_Operators/src/_60_Operator.java
@@ -0,0 +1,18 @@
+
+public class _60_Operator {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Java'da iki tam sayının bölümü tam sayı olarak verilir.
+		 * Eğer  bölümün kesir kısmını istiyorsak 
+		 * işleme giren değişkenlerden birini double veya float olarak tanımlamalıyız
+		 */
+		int Sayi1 = 10;
+		float Sayi2 = 3.0f; // 3 Sayısının küsüratlı yazışı
+		
+		System.out.println(Sayi1 / Sayi2);
+
+	}
+
+}
