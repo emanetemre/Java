81fbab6b224b30c4d6a0593e7ba9511fdb51aceb
emrekn <46498401+emrekn@users.noreply.github.com>
2019-01-31 23:05:54 +0300

    Add files via upload

---
+++ _10_HomeWork.java
@@ -0,0 +1,18 @@
+
+public class _10_HomeWork {
+
+	public static void main(String[] args) {
+		String Lamba1 = "Kirmizi";
+		String Lamba2 = "Sari";
+		String Lamba3 = "Yesil";
+		
+		if(Lamba3 == "Kirmizi")
+			System.out.println("Gecebilirsin");
+		else if(Lamba2 == "Kirmizi")
+			System.out.println("Hazir ol");
+		else
+			System.out.println("Lamba kirmizi bekle");
+
+	}
+
+}
---
+++ _20_HomeWork.java
@@ -0,0 +1,22 @@
+
+public class _20_HomeWork {
+
+	public static void main(String[] args) {
+		int yas = 17;
+		boolean erkek = false;
+		boolean kadin = false;
+		
+		if(erkek) {
+					System.out.println("erkeksin");
+				if(yas >= 18)
+					System.out.println("askerlik yasin gelmis");
+				else
+					System.out.println("askerlik yasin daha gelmedi");
+		}
+		else {
+		System.out.println("kadinsin");
+		System.out.println("askere gidemezsin");
+		}
+	}
+
+}
